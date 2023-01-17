8b0c29f9e12706b1822fe6618bd4fc789e02ff90
Kprzyby <kprzy@vp.pl>
2023-01-17 19:09:33 +0100

    now score=0 is assigned to a new user

--- ProjektZTP/Controllers/AccountController.cs
+++ ProjektZTP/Controllers/AccountController.cs
@@ -153,7 +153,7 @@ namespace ProjektZTP.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Score = 0 };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
