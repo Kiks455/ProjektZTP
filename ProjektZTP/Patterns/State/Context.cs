963dad0834a23543ee7937372880660450d7d448
Michał Ryniewicz <misiekxyz18@gmail.com>
2023-01-18 23:28:45 +0100

    state

--- ProjektZTP/Patterns/State/Context.cs
+++ ProjektZTP/Patterns/State/Context.cs
@@ -5,7 +5,43 @@ using System.Web;
 
 namespace ProjektZTP.Patterns.State
 {
+    public enum StateMode
+    {
+        Learning,
+        Test
+    }
+
     public class Context
     {
+        private State _state;
+
+        public Context()
+        {
+
+        }
+
+        public void ChangeState(StateMode state)
+        {
+            switch(state)
+            {
+                case StateMode.Learning:
+                    {
+                        _state = new LearningState();
+                        break;
+                    }
+                case StateMode.Test:
+                    {
+                        _state = new TestState();
+                        break;
+                    }
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(state), state, "This state is not implemented");
+            }
+        }
+
+        public State GetState()
+        {
+            return _state;
+        }
     }
 }
